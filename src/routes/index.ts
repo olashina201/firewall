6b6bf73c82183a64a1c05d2722520431ccfdbf4f
Olashina <69033389+olashina201@users.noreply.github.com>
2022-06-06 16:10:55 +0100

    add limit to endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { Register, Login } from "../handlers";
+import rateLimit from "express-rate-limit";
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message: "Too many attempts from this IP, please try again after 15 minutes",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
-const router = Router()
+const router = Router();
 
 router.post("/register", Register);
-router.post("/login", Login)
-
+router.post("/login", limiter, Login);
 
 export default router;
