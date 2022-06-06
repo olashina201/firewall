873077a68549019c52ba8d584240a13bc65807c4
Olashina <69033389+olashina201@users.noreply.github.com>
2022-06-06 16:51:13 +0100

    login attempt

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,9 @@ import { Register, Login } from "../handlers";
 import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  message: "Too many attempts from this IP, please try again after 15 minutes",
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 3, // Limit each IP to 3 requests per `window` (here, per 5 minutes)
+  message: "Too many attempts from this IP, please try again after 5 minutes",
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
