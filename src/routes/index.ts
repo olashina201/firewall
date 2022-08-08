c2685a4c147209823089cddc7f0d3fab84f6d625
quadri salawudeen <olamiquadri1@gmail.com>
2022-08-08 17:43:51 +0100

    cache error

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,6 @@ const router = Router();
 router.post("/register", Register);
 router.post("/login", Login);
 router.put("/updateAmt", UpdateAmt);
-router.put("/updateAmt", UpdateAmt);
 router.get("/getUser", getUser)
 router.put("/deleteUser", deleteUser);
 
